10afcbd57d378c2c0d06c126fb5abf14e4cdba87
Fafakofi <sfkbenson@st.ug.edu.gh>
2024-06-25 18:19:52 +0000

    user profile done

--- Home.js
+++ Home.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight} from 'react-native';
 
 
 export default function Home( ){
@@ -9,10 +9,35 @@ export default function Home( ){
             <View  style={styles.container1}>
 
                 <View style={styles.userProfile}>
-                    <View  style={styles.profilePic} >
-                        <Image source={require('./assets/userProfile.jpg')} style={styles.profileImg}/>
+                    <View style={styles.profileStuff}>
+
+                    
+                        <View  style={styles.profilePic} >
+                            <Image source={require('./assets/profile.png')} style={styles.profileImg}/>
+                        </View>
+    
+                        <View style={styles.profileText}>
+
+                            <Text style={styles.profileGreet}>
+                                Welcome back,
+                            </Text>
+
+                            <Text style={styles.profileName}>
+                            Eric Atsu
+                            </Text>
+
+                        </View>
+
                     </View>
+
+                    <View style={styles.profileSearchbar}>
+                        <TouchableHighlight style={styles.touch}>
+                            <Image source={require('./assets/search.png')}/>
+                        </TouchableHighlight>
+                    </View>
+
                 </View>
+
             </View>
         </View>
     )
@@ -30,6 +55,43 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     marginTop: 90,
     marginLeft:20,
+    marginRight: 20
   },
+
+  userProfile: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent:'space-between'
+  },
+
+  profileStuff: {
+    width: '50%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent:'space-between'
+  },
+
+  profileGreet: {
+    fontWeight: '200'
+  },
+
+  profileName: {
+    fontWeight: '800',
+    fontSize: 20
+  },
+
+  profileSearchbar: {
+    width: '10%',
+    height: '70%',
+    backgroundColor: '#D3D3D3',
+    borderRadius: 35
+  },
+
+  touch: {
+    top: 8,
+    left: 6
+  }
+
+  
 
 });
